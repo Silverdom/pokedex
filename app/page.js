fb9ddb50d4b86f2dcbab2eb0f8857113fecd1df0
SohamE <soham.584sengupta@gmail.com>
2023-11-18 13:02:05 +0530

    Adding metadatas to pages

--- app/page.js
+++ app/page.js
@@ -1,5 +1,4 @@
 
-import Navbar from '@/components/Navbar'
 import Banner from '@/components/Banner'
 import GlobalConfig from './app.config';
 import TopFive from '@/components/TopFive';
@@ -7,7 +6,6 @@ import TopFive from '@/components/TopFive';
 export default function Home() {
   return (
     <>
-      <Navbar />
       <Banner typeColor={GlobalConfig.color} />
       <TopFive typeColor={GlobalConfig.color} />
     </>
--- app/pokemon/[pokemonid]/page.js
+++ app/pokemon/[pokemonid]/page.js
@@ -1,11 +1,29 @@
 import { fetchSinglePokemonById } from "@/utils/fetchPokemonData";
 import Link from "next/link";
 
+let pokemonData = [];
+
+const getPokemonData = async (id) => {
+  if (!pokemonData[id]) {
+    pokemonData[id] = await fetchSinglePokemonById(id);
+  }
+  return pokemonData[id];
+}
+
+export async function generateMetadata({ params }) {
+  let pokemonDet = await getPokemonData(params.pokemonid);
+
+  return {
+    title: `Pokeinfo | ${pokemonDet.name}`,
+    description: `Description like sprite, stats, summary for ${pokemonDet.name}`
+  }
+}
+
 const pokemon = async ({ params }) => {
-  let pokemonData = await fetchSinglePokemonById(params.pokemonid);
+  let pokemonDet = await getPokemonData(params.pokemonid);
   return (
     <>
-      <p>This is pokemon det { pokemonData.name }</p>
+      <p>This is pokemon det { pokemonDet.name }</p>
       <Link href={ "/" }>Back</Link>
     </>
   );
