f1e801a68594064538979609b82e61c0fdf1987a
SohamE <soham.584sengupta@gmail.com>
2023-12-13 09:40:11 +0530

    Banner and responsive fixes

--- components/Banner.js
+++ components/Banner.js
@@ -8,8 +8,8 @@ const Banner = () => {
     <section className='banner c-bg-pdcol h-screen flex justify-evenly flex-col lg:flex-row text-center'>
       <div className='left-content grid content-center lg:w-1/2  m-auto text-white'>
         <h1 className='text-8xl'>PokeInfo</h1>
-        <p className='text-2xl mt-5'>The Ultimate Pokemon Encyclopedia!</p>
-        <AnimButton text="Let's Go" path="/search">Lets Go</AnimButton>
+        <p className='text-2xl mt-2'>The Ultimate Pokemon Encyclopedia!</p>
+        <AnimButton text="Lets Go" path="/search">Lets Go</AnimButton>
       </div>
       <div className='right-content hidden lg:block m-auto'>
         <Image alt="pokedex-icon" src="/pokedex-icon.png" width={ 534 } height={ 372 }></Image>
--- components/Buttons/AnimButton.js
+++ components/Buttons/AnimButton.js
@@ -12,9 +12,9 @@ const AnimButton = ({ children, text, path }) => {
   }
 
   return (
-    <div className='relative'>
+    <div className='relative mt-5'>
       <button
-        className='animbutton cursor-pointer border-2 border-white py-1 px-10 font-bold text-center overflow-hidden relative h-10 '
+        className='animbutton cursor-pointer border-2 border-white py-1 px-10 font-bold text-center overflow-hidden relative h-10 scale-150'
         onClick={navigateToPage}
       >
         <span data-text={ text } >
